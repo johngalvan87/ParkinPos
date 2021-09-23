f82bc08e74efe898593ce210ebf7689b231f0ceb
Windows <Windows@Gerencia>
2021-09-23 08:28:34 -0500

    primera version

--- src/Parkin/Constantes.java
+++ src/Parkin/Constantes.java
@@ -1,6 +1,10 @@
 package Parkin;
 
 public class Constantes {
-	
-
+	static final String RUTA_ARCHIVOS = "C:\\workspace\\PracticaPoo\\";
+	static final String BASE_DATOS = "BdParkin.txt";
+	static final int TARIFA_MOTO = 30;
+	static final int TARIFA_CARRO = 60;
+	static final int TARIFA_BICICLETA = 10;
+	static final int IVA_19 = 19;
 }
