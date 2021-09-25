19fc54b131e7c350528150ff4f4082d57136e0f7
Windows <Windows@Gerencia>
2021-09-24 19:32:27 -0500

    sdsd

--- src/Parkin/Facturar.java
+++ src/Parkin/Facturar.java
@@ -1,88 +1,17 @@
 package Parkin;
 
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Scanner;
 
 
 public class Facturar {
-	
-	static Scanner s;
-	static PlantillaFacturas f;
-	static Menu MenuOpciones;
-	static Bdparkin Bd;
-	static List<String> vehiculo;
-
 	public static void main(String[] args) {
-		s = new Scanner (System.in);
-		MenuOpciones = new Menu();
-		f = new PlantillaFacturas();
-				
-		int operacion = Menu.menuOperaciones();
-		int tipoVehiculo;		
-		int impFactura = 1;
-		int tarifa = 0;
-		int carro = 0;
-		int moto = 0;
-		int bicicleta;
-		int hora =0;
-		int minuto =0;	
-		String placa;
-		String nuevoVehiculo,fraVehiculo;
-		String horaIngreso,horaSalida = (hora+":"+minuto);
 		
-
-		
-		operacion = Menu.menuOperaciones();
-		
-		while (operacion >= 1 && operacion <=3) {
-			if (operacion == 1) {
-				System.out.println("Digite los datos solicitados: \n");
-				tipoVehiculo = Menu.menuTarifa();
-				System.out.println("Digite la placa O Numero de tarjeta propiedad para las bicicletas: \n");
-				placa = s.next();
-				System.out.println("Hora de ingreso: ");
-				horaIngreso = s.next();
-				if (tipoVehiculo == 1) {
-					nuevoVehiculo = ("Carro"+placa+"-"+horaIngreso);
-					switch (operacion) {
-					case 1:
-						Bd.ingresarVehiculo(Constantes.RUTA_ARCHIVOS,Constantes.BASE_DATOS,nuevoVehiculo);
-						
-						break;
-					case 2:
-						nuevoVehiculo = ("Moto"+placa+"-"+horaIngreso);
-						Bd.ingresarVehiculo(Constantes.RUTA_ARCHIVOS,Constantes.BASE_DATOS,nuevoVehiculo);
-						
-						break;
-						
-					case 3:						
-						nuevoVehiculo = ("Bicicleta"+placa+"-"+horaIngreso);
-						Bd.ingresarVehiculo(Constantes.RUTA_ARCHIVOS,Constantes.BASE_DATOS,nuevoVehiculo);
-						
-						break;
-						
-						}
-				}
-			}
-			if (operacion == 2) {
-				System.out.println("Digite la hora de salida: ");
-				horaSalida = s.next();
-				vehiculo = Bd.consultarBd(Constantes.RUTA_ARCHIVOS,Constantes.BASE_DATOS);
-			}
-			int placa = Integer(placa);
-			fraVehiculo = vehiculo.get(placa);
-			System.out.println(fraVehiculo);
-		}
-				
-		f.imprimirFactura(impFactura);
+		PlantillaFacturas factura = new PlantillaFacturas();
+		System.out.println(factura);
 		
 		
-
 	}
-
-	private static int Integer(int placa) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
+		
 }
--- src/Parkin/PlantillaFacturas.java
+++ src/Parkin/PlantillaFacturas.java
@@ -1,30 +1,23 @@
 package Parkin;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Date;
+import java.util.Scanner;
+
+import java.time.format.DateTimeFormatter;
 
 public class PlantillaFacturas {
-	static Date f;			
-		public void imprimirFactura(int impFactura) {
-			
-			f = new Date ();				
-			String razonSocial = "Parkin City";
-			String nit = "901279765-2";
-			String telefono ="(601) 6904563";
-			String direccion ="Cra 15 # 118 - 41";
-			int fDia = f.getDate();
-			int fMes= f.getMonth();
-			int fAno = f.getYear();
-		
-			switch (impFactura) {
-			case 1:			
-			System.out.println("	!		"+razonSocial+"		!");
-			System.out.println("	!	      Nit."+nit+"		!");
-			System.out.println("	!	       "+telefono+"		!");
-			System.out.println("	!	 Factura de Venta No:"+direccion+"	!");
-			System.out.println("	!	 Direccion:"+direccion+"	!");
-			System.out.println("	!	   Fecha Factura:"+fDia+"/"+fMes+"/"+fAno+"	!");		
-			System.out.println("	!=======================================!	");
-			
-			}
-		}
+	static SimpleDateFormat formato;
+	static Scanner s;			
+	
+	public static int factura() {
+		int facturar = 1;
+	
+		return facturar;
 	}
+	
+}
+			
+
